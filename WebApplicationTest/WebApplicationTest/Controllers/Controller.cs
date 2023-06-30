e08cbb62636f790c17e6604b363a7609b0bf4537
avonavia <121552637+avonavia@users.noreply.github.com>
2023-06-30 19:19:30 +0300

    Add files via upload

---
+++ WebApplicationTest/WebApplicationTest/Controllers/Controller.cs
@@ -0,0 +1,49 @@
+using ExampleService.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ExampleService.Controllers
+{
+    public class Controller : ControllerBase
+    {
+        List<ColoredAlienCat> catList = new List<ColoredAlienCat>();
+
+        [HttpPost]
+        public string NoCats()
+        {
+            string sadMessage = "No cats here(";
+            return sadMessage;
+        }
+
+        [HttpGet, Route("cats")]
+        public IEnumerable<ColoredAlienCat> FormCatList()
+        {
+            MakeCats();
+            return catList;
+        }
+
+        [HttpGet, Route("cats/{id}")]
+        public IEnumerable<ColoredAlienCat> GetCatByID(int id)
+        {
+            MakeCats();
+            List<ColoredAlienCat> list = new List<ColoredAlienCat>();
+            foreach (var item in catList)
+            {
+                if (item.Id == id)
+                {
+                    list.Add(item);
+                }
+            }
+            return list;
+        }
+
+        public void MakeCats()
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                ColoredAlienCat cat = new ColoredAlienCat(i + 1);
+                catList.Add(cat);
+            }
+        }
+    }
+}
---
+++ WebApplicationTest/WebApplicationTest/Models/ColoredAlienCat.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExampleService.Model
+{
+    public class ColoredAlienCat
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public ColoredAlienCat(int id)
+        {
+            Random randomColor = new Random();
+            Random randomLetter = new Random();
+
+            List<string> colors = new List<string> { "Green", "Red", "Blue", "Orange", "Yellow", "Purple", "Pink", "Black", "White" };
+            string letters = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            for (int i = 0; i < 10; i++)
+            {
+                string name = string.Empty;
+                int colorID = randomColor.Next(colors.Count);
+
+                for (var j = 0; j < 7; j++)
+                {
+                    int num = randomLetter.Next(0, letters.Length);
+                    name += letters[num];
+                }
+
+                Id = id;
+                Color = colors[colorID];
+                Name = name;
+            }
+        }
+    }
+}
